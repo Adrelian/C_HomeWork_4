12d55d07ac5f5042aedd5e2a3b5a0838b2cf19af
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-27 00:08:25 +0500

    решил задачу 2, возможно есть способ проще

--- Homework 4.cs
+++ Homework 4.cs
@@ -3,23 +3,23 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-void Logorifm()
-{
-    Console.Write("Введите число: ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите степень: ");
-    int degree = Convert.ToInt32(Console.ReadLine());
+// void Logorifm()
+// {
+//     Console.Write("Введите число: ");
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Введите степень: ");
+//     int degree = Convert.ToInt32(Console.ReadLine());
 
-    int result = 1;
-    int count = 1; 
-    while (count <= degree)
-    {
-        result = result*number;
-        count++;
-    }
-    Console.WriteLine($"{number}^{degree} = {result}");
-}
-Logorifm();
+//     int result = 1;
+//     int count = 1; 
+//     while (count <= degree)
+//     {
+//         result = result*number;
+//         count++;
+//     }
+//     Console.WriteLine($"{number}^{degree} = {result}");
+// }
+// Logorifm();
 
 
 
@@ -29,7 +29,26 @@ Logorifm();
 // 82 -> 10
 // 9012 -> 12
 
+void findSumNumber()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    {
+        // Дробив число справа на лево и складываем эти числа
+        int sum = 0; //сумма цифр
+        int newNumber = number; //начало отсчета
+        while (number > 0)
+        {
+            int rightNumber = number % 10; //правое число (1234 -> 4)
+            newNumber = number; // оставшееся число (123)
+            number = number / 10; //Уменьшаем кол-во разрядов (1234 -> 123)
+            sum = sum + rightNumber; //общая сумма (0 + 4)
+        }
+        Console.WriteLine($"Сумма цифр: {sum}");
+    }
+}
 
+findSumNumber();
 
 
 
